510267ba1b1d33ebafba6952ae07d7ae41cbe769
Jason Cale <jase@gofreerange.com>
2011-04-08 17:26:35 +0100

    Added a 'day challenge's to the achievements

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,7 +2,12 @@ class Achievement < ActiveRecord::Base
   has_and_belongs_to_many :users
   
   @achievement_conditions = {
-    "30 Day Challenge" => Proc.new { |user| user.longest_streak >= 30 }
+    "30 Day Challenge" => Proc.new { |user| user.longest_streak >= 30 },
+    "60 Day Challenge" => Proc.new { |user| user.longest_streak >= 60 },
+    "90 Day Challenge" => Proc.new { |user| user.longest_streak >= 60 },
+    "101 Day Challenge" => Proc.new { |user| user.longest_streak >= 101 },
+    "Six Month Challenge" => Proc.new { |user| user.longest_streak >= 180 },
+    "365 Day Challenge" => Proc.new { |user| user.longest_streak >= 30 }
   }
   
   class << self    
--- test/unit/achievement_test.rb
+++ test/unit/achievement_test.rb
@@ -19,5 +19,71 @@ class AchievementTest < ActiveSupport::TestCase
         assert @user.achievements.map(&:name).include?("30 Day Challenge")
       end
     end
+    
+    context "who does 60 straight days of yoga" do
+      setup do
+        Factory.create(:achievement, :name => "60 Day Challenge")
+        
+        streak_for @user, 60.days
+        @user.update_progress(@user.lessons)
+      end
+
+      should "get the 30 day challenge achievement" do
+        assert @user.achievements.map(&:name).include?("60 Day Challenge")
+      end
+    end
+    
+    context "who does 90 straight days of yoga" do
+      setup do
+        Factory.create(:achievement, :name => "90 Day Challenge")
+        
+        streak_for @user, 90.days
+        @user.update_progress(@user.lessons)
+      end
+
+      should "get the 90 day challenge achievement" do
+        assert @user.achievements.map(&:name).include?("90 Day Challenge")
+      end
+    end
+    
+    context "who does 101 straight days of yoga" do
+      setup do
+        Factory.create(:achievement, :name => "101 Day Challenge")
+        
+        streak_for @user, 101.days
+        @user.update_progress(@user.lessons)
+      end
+
+      should "get the 101 day challenge achievement" do
+        assert @user.achievements.map(&:name).include?("101 Day Challenge")
+      end
+    end
+    
+    context "who does 6 months of continous yoga" do
+      setup do
+        Factory.create(:achievement, :name => "Six Month Challenge")
+        
+        streak_for @user, 6.months
+        @user.update_progress(@user.lessons)
+      end
+
+      should "get the Six Month Challenge achievement" do
+        assert @user.achievements.map(&:name).include?("Six Month Challenge")
+      end
+    end
+    
+    context "who does 1 year of continous yoga" do
+      setup do
+        Factory.create(:achievement, :name => "365 Day Challenge")
+        
+        streak_for @user, 1.year.to_i
+        @user.update_progress(@user.lessons)
+      end
+
+      should "get the 365 day challenge achievement" do
+        assert @user.achievements.map(&:name).include?("365 Day Challenge")
+      end
+    end
+    
   end
 end
