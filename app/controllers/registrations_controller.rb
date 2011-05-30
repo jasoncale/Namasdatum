17028c3ad0f90e26aac8bb1970958a6bf5a627f4
Jason Cale <jase@gofreerange.com>
2011-05-31 00:40:50 +0100

    Removed unneeded sessions controller and added auth requirement to foursquare callback

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,19 @@
 require 'foursquare'
 
 class RegistrationsController < Devise::RegistrationsController
+  
+  before_filter :authenticate_user!, :only => [:foursquare_callback]
+  
   def edit
     unless current_user && current_user.foursquare_access_token.present?
       @authorize_url = foursquare.authorize_url(foursquare_callback_url)    
     end
   end
   
-  def foursquare_callback
-    code = params[:code]
-    current_user.update_attribute(:foursquare_access_token, foursquare.access_token(code, foursquare_callback_url))
+  def foursquare_callback    
+    if params[:code]
+      current_user.update_attribute(:foursquare_access_token, foursquare.access_token(params[:code], foursquare_callback_url))
+    end
     redirect_to edit_user_registration_path
   end
   
